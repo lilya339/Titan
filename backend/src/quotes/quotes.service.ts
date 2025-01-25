83d6f29b3d9ba5e2060a32dd73b21b9de726f5bc
Liliya3DOptix <liliyas@3doptix.com>
2025-01-25 23:04:52 +0200

    remove unused function

--- backend/src/quotes/quotes.service.ts
+++ backend/src/quotes/quotes.service.ts
@@ -52,37 +52,5 @@ export class QuotesService {
         const quotes = await this.quoteDAOService.aggregate(pipeline);
         return quotes;
     }
-
-    public async fillQuotesToDB() {
-        const LIMIT = 200;
-        for (let i = 0; i < LIMIT; i++) {
-            const URL = 'https://favqs.com/api/qotd';
-
-            axios.get(URL).then(async (response) => {
-                this.addNewQuote(response.data.quote);
-            }).catch((error) => {
-                console.error(error);
-            });
-        }
-    }
-
-    private async addNewQuote(quote: iQuote) {
-        try {
-
-            const exists = await this.quoteDAOService.findOne({ favqs_id: quote.id });
-            if (exists) {
-                return;
-            }
-
-            const newQuote = this.quoteDAOService.create({
-                ...quote,
-                favqs_id: quote.id
-            });
-            await newQuote.save();
-
-        } catch (error) {
-            console.log("failed create quote")
-        }
-    }
 }
 
